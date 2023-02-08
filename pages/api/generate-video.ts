91ab64ae84c2fec32de01483f1b34dcc7f1294e4
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2023-02-08 14:46:00 +0100

    fix types

--- pages/api/generate-video.ts
+++ pages/api/generate-video.ts
@@ -3,13 +3,14 @@ import { createClient } from "pexels";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const client = createClient(process.env.PEXELS_API_KEY || "");
-	const query = req.query.topic;
+	const query = req.query.topic as string;
 	const orientation = "portrait";
 
 	let videoURL = "";
 
 	await client.videos.search({ query, per_page: 1, orientation: orientation }).then((videos) => {
-		videoURL = videos.videos[0].video_files[0].link;
+    // @ts-ignore
+    videoURL = videos.videos[0].video_files[0].link;
 	});
 
 	res.status(200).json(videoURL);
